0c41663b7db5d6633b7b4151c2224e441abacb06
Sara Greenwald <you@example.com>
2026-06-16 21:34:12 -0400

    added .gitignore file and c# files

---
+++ html/48/Asssessment/Shirts.cs
@@ -0,0 +1,24 @@
+﻿namespace Asssessment {
+    internal class Shirts {
+
+        List<string> colors = new List<string>() {
+            "red",
+            "green",
+            "blue"
+        };
+        List<string> patterns = new List<string>() {
+            "striped",
+            "checked",
+            "plain"
+
+        };
+        public void PrintShirts() {
+            foreach (string color in colors) {
+                foreach (string pattern in patterns) {
+                    Console.WriteLine($"{color}: {pattern}");
+                }
+            }
+        }
+
+    }
+}
---
+++ html/48/Asssessment/UsingShirts.cs
@@ -0,0 +1,8 @@
+﻿namespace Asssessment {
+    internal class UsingShirts {
+        static void Main(string[] args) {
+            Shirts shirts = new Shirts();
+            shirts.PrintShirts();
+        }
+    }
+}
